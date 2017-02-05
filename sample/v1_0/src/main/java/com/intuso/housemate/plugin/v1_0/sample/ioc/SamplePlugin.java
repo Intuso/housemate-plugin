d7b94a6fc7007c0156296ae668edadac84a4b1a3
Tom Clabon <tom@intuso.com>
2017-02-05 16:09:12 +0000

    Removed features. Each device now has a few sets of different properties which are object references to connected devices

--- sample/v1_0/src/main/java/com/intuso/housemate/plugin/v1_0/sample/ioc/SamplePlugin.java
+++ sample/v1_0/src/main/java/com/intuso/housemate/plugin/v1_0/sample/ioc/SamplePlugin.java
@@ -4,18 +4,15 @@ import com.google.inject.Inject;
 import com.intuso.housemate.client.v1_0.api.annotation.Id;
 import com.intuso.housemate.client.v1_0.api.plugin.AnnotatedPlugin;
 import com.intuso.housemate.client.v1_0.api.plugin.ConditionDrivers;
-import com.intuso.housemate.client.v1_0.api.plugin.FeatureDrivers;
 import com.intuso.housemate.client.v1_0.api.plugin.TaskDrivers;
 import com.intuso.housemate.plugin.v1_0.sample.CustomArg;
 import com.intuso.housemate.plugin.v1_0.sample.condition.DaylightCondition;
-import com.intuso.housemate.plugin.v1_0.sample.feature.CustomFeatureDriver;
 import com.intuso.housemate.plugin.v1_0.sample.task.DoYourThingTask;
 import com.intuso.utilities.properties.api.WriteableMapPropertyRepository;
 
 @Id(value = "sample-plugin",
         name = "Sample plugin",
         description = "Sample Plugin to show how to register new types etc")
-@FeatureDrivers({CustomFeatureDriver.class}) // devices
 @ConditionDrivers({DaylightCondition.class}) // conditions
 @TaskDrivers({DoYourThingTask.class}) // tasks
 public class SamplePlugin extends AnnotatedPlugin {
